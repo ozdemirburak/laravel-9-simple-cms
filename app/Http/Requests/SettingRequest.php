e30dac92d1a735886c641098fa3903157f4bbe7c
ozdemirbur <mail@burakozdemir.co.uk>
2015-06-18 05:19:36 +0300

    fix indent

--- app/Http/Requests/SettingRequest.php
+++ app/Http/Requests/SettingRequest.php
@@ -6,29 +6,29 @@ use App\Http\Requests\Request;
 
 class SettingRequest extends Request
 {
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
-		return true;
-	}
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
 
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array
-	 */
-	public function rules()
-	{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
         return [
             'email'	=> 'required|email|min:7',
             'facebook' => 'sometimes|min:21|max:200',
             'twitter' => 'sometimes|min:18|max:200',
             'logo' => 'sometimes|max:2048|image'
         ];
-	}
+    }
 
 }
