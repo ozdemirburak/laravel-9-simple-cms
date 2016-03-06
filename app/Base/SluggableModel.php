aaa7f626f1e7182aac1aa01ccc1986474ece09b5
Burak Ozdemir <burakozdemir@protonmail.ch>
2016-03-06 06:41:21 +0200

    Create sluggable model, DRY

---
+++ app/Base/SluggableModel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Base;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
+
+class SluggableModel extends Model implements SluggableInterface
+{
+    use SluggableTrait;
+
+    /**
+     * Create slug from title using 3rd party trait
+     *
+     * @var array
+     */
+    protected $sluggable = array(
+        'build_from' => 'title',
+        'save_to'    => 'slug',
+        'on_update'  => true
+    );
+
+    /**
+     * Set the slug according to Turkish language (Ö => o and Ü => u) instead of German (Ö => oe and Ü => ue)
+     *
+     * @param $slug
+     */
+    public function setSlugAttribute($slug)
+    {
+        $this->attributes['slug'] = str_replace(["oe", "ue"], ["o", "u"], $slug);
+    }
+}
