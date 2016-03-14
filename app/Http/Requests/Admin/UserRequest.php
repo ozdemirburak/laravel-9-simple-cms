7f7d7f78ea355296b9395115f9f39f48e4888409
Burak Ozdemir <burakozdemir@protonmail.ch>
2016-03-14 05:16:00 +0200

    DRY code

--- app/Http/Requests/Admin/UserRequest.php
+++ app/Http/Requests/Admin/UserRequest.php
@@ -3,21 +3,9 @@
 namespace App\Http\Requests\Admin;
 
 use App\Http\Requests\Request;
-use App\User;
 
 class UserRequest extends Request
 {
-
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
