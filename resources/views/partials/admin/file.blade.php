bd6aea3c3e30787645fa59fcf39f828244128097
Burak Ozdemir <burakozdemir@protonmail.ch>
2016-05-25 13:15:12 +0300

    Fix uploaded path rendering

--- resources/views/partials/admin/file.blade.php
+++ resources/views/partials/admin/file.blade.php
@@ -1,5 +1,7 @@
 @unless (trim($file) === "")
     <div class="uploaded-file">
-        <a title="{{ trans('admin.fields.uploaded')  }}" href="{!! $file !!}" target="_blank"> <i class="fa fa-4x fa-file"></i></a>
+        <a title="{{ trans('admin.fields.uploaded')  }}" href="{{ asset($file) }}" target="_blank">
+            <i class="fa fa-4x fa-file"></i>
+        </a>
     </div>
 @endunless
