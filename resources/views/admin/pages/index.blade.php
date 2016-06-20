dabe29cdde897a4dde1f27bea37b7e4ef6c66e00
Burak Ozdemir <burakozdemir@protonmail.ch>
2016-06-20 17:48:34 +0300

    Use sortable as partial

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -1,36 +1,5 @@
 @extends('layouts.admin')
 
 @section('content')
-    @if(count($object))
-        <div class="dd">
-            <ol class="dd-list">
-                @foreach($object as $node)
-                    {!! renderNode($node, "page") !!}
-                @endforeach
-            </ol>
-        </div>
-        <script>
-            $(function () {
-                $('.dd').nestable({
-                    callback: function(){
-                        $.ajax({
-                            type: 'POST',
-                            url: '{{ route('admin.page.order') }}',
-                            data: JSON.stringify($('.dd').nestable('asNestedSet')),
-                            contentType: "json",
-                            headers: {
-                                'X-CSRF-Token': $('meta[name="_token"]').attr('content')
-                            },
-                            error:  function (xhr, ajaxOptions, thrownError) {
-                                console.log(xhr.status);
-                                console.log(thrownError);
-                            }
-                        });
-                    }
-                });
-            });
-        </script>
-    @else
-        {{ trans('admin.empty') }}
-    @endif
+    @include('partials.admin.sortable', ['resource' => 'page'])
 @endsection
