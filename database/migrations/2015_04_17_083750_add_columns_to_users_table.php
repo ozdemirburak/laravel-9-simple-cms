ad2f3ae00a5f0f2a1998551bcd15ef0ca0da7e64
gusilveiramp <gdsmp@hotmail.com>
2016-09-05 17:31:45 -0300

    Update 2015_04_17_083750_add_columns_to_users_table.php
    Hi Friend! I saw that you upgraded the project to Laravel 5.3! That's nice!
    During the installation process, on the 4th step of the Quick Install Guide ($ php artisan migrate), an error appears because of the timestamp fields () are blank. Adding 'nullable()' function, the problem is solved.

--- database/migrations/2015_04_17_083750_add_columns_to_users_table.php
+++ database/migrations/2015_04_17_083750_add_columns_to_users_table.php
@@ -13,8 +13,8 @@ class AddColumnsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('logged_in_at');
-            $table->timestamp('logged_out_at');
+            $table->timestamp('logged_in_at')->nullable();
+            $table->timestamp('logged_out_at')->nullable();
             $table->string('ip_address', 45)->index();
             $table->string('picture');
         });
