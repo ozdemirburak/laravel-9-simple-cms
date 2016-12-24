32c89739d07d89babc75ba749afbfa7e2e141365
Burak Ozdemir <burakozdemir@protonmail.ch>
2016-12-24 15:14:49 +0300

    Make toMail method compatible with the base class

--- app/Base/Auth/ResetPassword.php
+++ app/Base/Auth/ResetPassword.php
@@ -10,9 +10,11 @@ class ResetPassword extends BaseResetPassword
     /**
      * Build the mail representation of the notification.
      *
+     * @param mixed $notifiable
+     *
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
-    public function toMail()
+    public function toMail($notifiable)
     {
         $message = (new MailMessage)
             ->subject(trans('auth.password.email.action'))
