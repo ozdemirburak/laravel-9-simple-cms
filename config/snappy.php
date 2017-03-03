8c5d3d4242ef812034b153ebde6aba3d315b38c2
Burak Ozdemir <burakozdemir@protonmail.ch>
2017-03-03 10:27:45 +0300

    Changed array notation on snappy config

--- config/snappy.php
+++ config/snappy.php
@@ -1,22 +1,18 @@
 <?php
 
-return array(
-
-
-    'pdf' => array(
+return [
+    'pdf' => [
         'enabled' => true,
         'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         'timeout' => false,
-        'options' => array(),
-        'env'     => array(),
-    ),
-    'image' => array(
+        'options' => [],
+        'env'     => [],
+    ],
+    'image' => [
         'enabled' => true,
         'binary'  => base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64'),
         'timeout' => false,
-        'options' => array(),
-        'env'     => array(),
-    ),
-
-
-);
+        'options' => [],
+        'env'     => [],
+    ],
+];
