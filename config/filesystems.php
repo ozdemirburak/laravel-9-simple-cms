4e7e6c1335878ea225760f13d28068f1f1d360f3
Burak Ozdemir <burakozdemir@protonmail.ch>
2017-09-20 00:12:24 +0300

    Added uploads disk back.

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'uploads' => [
+            'driver' => 'local',
+            'root'   => public_path('uploads'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
