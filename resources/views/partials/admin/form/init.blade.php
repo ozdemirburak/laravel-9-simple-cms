9f957904df63444d66e079ae759e5490c01084df
Burak Ozdemir <burakozdemir@protonmail.ch>
2018-10-29 00:19:52 +0300

    set form enctype for possible file uploading problems

--- resources/views/partials/admin/form/init.blade.php
+++ resources/views/partials/admin/form/init.blade.php
@@ -2,6 +2,6 @@
     <div class="container">
         <h1 class="title">{{ __('admin.' . $resource . '.' . ($action ?? 'edit')) }}</h1>
         @php [$name, $id] = ${$resource} !== null ? ['update', ${$resource}->id] : ['store', null] @endphp
-        <form method="POST" action="{{ route('admin.' . $resource . '.' . $name, $id) }}">
+        <form method="POST" enctype="multipart/form-data" action="{{ route('admin.' . $resource . '.' . $name, $id) }}">
         @include('partials.admin.errors')
 
