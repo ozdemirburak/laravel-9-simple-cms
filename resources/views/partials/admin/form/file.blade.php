7fd552e55a9b1f5b8debbfa8fd85af9b4f890e35
Burak Ozdemir <burakozdemir@protonmail.ch>
2019-01-23 16:59:43 +0300

    fix icon with the upload filed

--- resources/views/partials/admin/form/file.blade.php
+++ resources/views/partials/admin/form/file.blade.php
@@ -3,7 +3,7 @@
         <label class="file-label">
             <input class="file-input" type="file" name="{{ $attribute }}">
             <div class="file-cta">
-                <div class="file-icon">{!! icon('cloud-upload') !!}</div>
+                <div class="file-icon">{!! icon('upload-cloud') !!}</div>
                 <div class="file-label">{{ __('admin.fields.' . $attribute) }}</div>
             </div>
             <div class="file-name is-hidden"></div>
